424991dd612a18b8b638649a65c267647e194749
Gabriella Sinka-Pálinkás <fusewent@gmail.com>
2018-12-02 15:24:35 +0100

    small fixes

--- app/src/main/java/com/example/cressida/slidingpuzzleapp/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/cressida/slidingpuzzleapp/LoginRegisterActivity.kt
@@ -3,19 +3,7 @@ package com.example.cressida.slidingpuzzleapp
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v4.content.ContextCompat
-import android.support.v7.app.AppCompatActivity
-import android.text.Editable
-import android.text.TextUtils
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
 import com.example.cressida.slidingpuzzleapp.logic.FireAuth
-import com.example.cressida.slidingpuzzleapp.views.PlayActivity
-import com.google.firebase.auth.FirebaseAuth
-
-import kotlinx.android.synthetic.main.activity_login_register.*
 
 class LoginRegisterActivity : Activity() {
 
---
+++ app/src/main/java/com/example/cressida/slidingpuzzleapp/PlayActivity.kt
@@ -0,0 +1,29 @@
+package com.example.cressida.slidingpuzzleapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_play.*
+
+class PlayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play)
+
+        playButton.setOnClickListener {
+            val intent = Intent( this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        settingsButton.setOnClickListener {
+            val intent = Intent( this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        leaderboardButton.setOnClickListener {
+            val intent = Intent( this, LeaderbordActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
