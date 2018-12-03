cea6ca4502db9706a572870790b982913ff9b272
Fangee <mozes.daniel92@gmail.com>
2018-12-04 00:59:49 +0100

    setting IntroActivity as launcher activity

--- app/src/main/java/com/example/cressida/slidingpuzzleapp/MainActivity.kt
+++ app/src/main/java/com/example/cressida/slidingpuzzleapp/MainActivity.kt
@@ -17,8 +17,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent(this, PlayActivity::class.java)
-        startActivity(intent)
 
     }
 
