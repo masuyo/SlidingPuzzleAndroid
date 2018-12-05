e646803452ea9040e406c375423a4edd4ece9b56
Gabriella Sinka-Pálinkás <fusewent@gmail.com>
2018-12-05 18:44:52 +0100

    fix

--- app/src/main/java/com/example/cressida/slidingpuzzleapp/LeaderboardActivity.kt
+++ app/src/main/java/com/example/cressida/slidingpuzzleapp/LeaderboardActivity.kt
@@ -1,19 +1,13 @@
 package com.example.cressida.slidingpuzzleapp
 
-import android.graphics.Color
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.GridLayoutManager
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import com.example.cressida.slidingpuzzleapp.R
 import com.example.cressida.slidingpuzzleapp.logic.FireDB
 import com.example.cressida.slidingpuzzleapp.logic.LeaderboardAdapter
 import com.example.cressida.slidingpuzzleapp.model.LeaderboardData
-import com.google.firebase.firestore.DocumentSnapshot
-import com.google.firebase.firestore.EventListener
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.QuerySnapshot
 import kotlinx.android.synthetic.main.activity_leaderboard.*
 
 class LeaderboardActivity : AppCompatActivity(), FireDB.IFireCallback {
@@ -24,16 +18,12 @@ class LeaderboardActivity : AppCompatActivity(), FireDB.IFireCallback {
         rv_leaderboard.adapter = LeaderboardAdapter(data)
     }
 
-
-    val fireDB = FireDB()
-
-
     override fun onStart() {
         super.onStart()
 
         var mDocRef = FirebaseFirestore.getInstance().collection("users")
 
-        fireDB.GetAllHighScore(this)
+        FireDB.GetAllHighScore(this)
 
 
     }
