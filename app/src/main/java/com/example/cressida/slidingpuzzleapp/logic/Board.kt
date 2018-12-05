d5b1e636da57086daed9668bf8bbf7cce337a44c
DESKTOP-IG045IF\Dezso <disznosajt23@gmail.com>
2018-12-05 18:35:44 +0100

    pontszám továbbitása

--- app/src/main/java/com/example/cressida/slidingpuzzleapp/logic/Board.kt
+++ app/src/main/java/com/example/cressida/slidingpuzzleapp/logic/Board.kt
@@ -11,6 +11,7 @@ class Board(mapstring: String) : Observable() {
     var actualStep: Int = 0
     var table: ArrayList<Block> = ArrayList()
     private val pruposex = 5
+    var mapName = ""
     val exit: Block = Block(5,2,1,false)
     init {
         loadMap(mapstring)
@@ -22,7 +23,8 @@ class Board(mapstring: String) : Observable() {
         var map = mapstring.split(',')
         try {
             minStep = map[0].toInt()
-            for (i in 1 until map.size) {
+            mapName = map[1]
+            for (i in 2 until map.size) {
                 var block = map[i].split(' ')
                 var vertical = false
                 if (block[3] == "true")
@@ -51,7 +53,7 @@ class Board(mapstring: String) : Observable() {
     fun gameOver() {
 
         var point = ((minStep.toDouble()/actualStep.toDouble())*100).toInt()
-
+        FireDB.UploadScore(mapName,point)
         setChanged()
         notifyObservers(true)
     }
